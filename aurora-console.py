2f8da49a02d7e619d34950e979cb726303227315
iamtheone188 <kevinhan188@gmail.com>
2013-07-10 14:49:17 -0400

    Initial Commit for Aurora console

---
+++ aurora-console.py
@@ -0,0 +1,11 @@
+# Aurora Console
+# SAVI Mcgill: Heming Wen, Prabhat Tiwary, Kevin Han, Michael Smith
+
+"""
+Command-line interface to the Aurora API
+"""
+
+import argparse
+
+class AuroraArgumentParser(argparse.ArgumentParser):
+    
