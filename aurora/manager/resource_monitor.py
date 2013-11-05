38fb253f59b981799d7bb8f21aac694d3a188055
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-11-05 15:35:25 -0500

    Wrote restart function

--- aurora/manager/resource_monitor.py
+++ aurora/manager/resource_monitor.py
@@ -14,12 +14,7 @@ class resourceMonitor():
         # the AP's OS has crashed, or at least aurora is
         # no longer running.
         
-        # TODO: DATABASE
-        # Identify AP from unique_id of command
-        # for all slices and/or commands relating to AP:
-        #   if slice is active, mark down
-        #   else if slice is deleting, mark deleted (will be when we reinitialize)
-        #   else if slice is pending, mark failed
+        # use set_status
         
         # In the future we might do something more with the unique_id besides
         # identifying the AP, like log it to a list of commands that cause
@@ -31,22 +26,28 @@ class resourceMonitor():
         
     
     
-    def set_status(self, unique_id, success):
+    def set_status(self, unique_id, success, ap_up=True):
         """Sets the status of the associated request in the
         database based on the previous status, i.e. pending -> active if
         create slice, deleting -> deleted if deleting a slice, etc."""
         print("SETTING STATUS OF " + str(unique_id) + " to " + str(success))
         # TODO: DATABASE
         # Identify slice by unique_id
-        # if pending and success, mark active
-        # else if deleting and success, mark deleted
-        # else if pending and failed, mark failed
-        # else if deleting and failed, mark failed (forcing user
+        # if ap_up:
+        #   if pending and success, mark active
+        #   else if deleting and success, mark deleted
+        #   else if pending and failed, mark failed
+        #   else if deleting and failed, mark failed (forcing user
         # to try deleting again or contact admin saying I can't delete;
         # this situation is so unlikely that if it happens an admin
         # really should come by and see what's going on)
+        # else :
+        # for all slices and/or commands relating to AP:
+        #   if slice is active, mark down
+        #   else if slice is deleting, mark deleted (will be when we reinitialize)
+        #   else if slice is pending, mark failed
         
         
     def reset_AP(self, ap):
-        print("RESETTING " + str(ap))
-        pass
+        self.dispatcher.dispatch( { 'slice' : 'admin', 'command' : 'restart' } , ap, 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF')
+        
