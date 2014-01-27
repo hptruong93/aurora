26d8e4fbe78546eca3329edf538c2fefbaf0960b
Mike.Kobierski <michael.kobierski@mail.mcgill.ca>
2014-01-27 14:27:16 -0500

    Set default controller to manager public IP

--- aurora/manager/plugins/OvsPlugin.py
+++ aurora/manager/plugins/OvsPlugin.py
@@ -8,8 +8,7 @@ class OvsPlugin():
     def __init__(self, tenant_id):
         self.tenant_id = tenant_id
         self.bridge_attributes = {
-            #'controller':{'listable':True, 'default':'tcp:132.206.206.133'},
-            'controller':{'listable':True, 'default':''},
+            'controller':{'listable':True, 'default':'tcp:132.206.206.133:6633'},
             'dpid':{'listable':True, 'default':self.default_dpid()}
         }
         self.attributes = {
