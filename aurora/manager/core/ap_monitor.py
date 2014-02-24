5468b3202df0790136cb3111b19a4d33b2ce9b41
Mike.Kobierski <michael.kobierski@mail.mcgill.ca>
2014-02-24 15:19:01 -0500

    Update ap_monitor.py

--- aurora/manager/core/ap_monitor.py
+++ aurora/manager/core/ap_monitor.py
@@ -275,7 +275,7 @@ class APMonitor(object):
                 else:
                     time_active = datetime.timedelta(seconds=0)
                 cur.execute("UPDATE ap_slice SET time_active=%s, last_active_time=Now() WHERE ap_slice_id='%s'" %
-                            (self.get_time_format(time_active), str(unique_id)))
+                            (self.get_time_format(time_active), str(ap_slice_id)))
         except Exception:
             traceback.print_exc(file=sys.stdout)
             #self.LOGGER.error("Error: %s", str(e))
