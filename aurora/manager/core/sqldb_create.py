069059c102a912fa9305f26b56b0c27df6fc9777
Mike.Kobierski <michael.kobierski@mail.mcgill.ca>
2014-02-26 12:26:18 -0500

    Update sqldb_create.py

--- aurora/manager/core/sqldb_create.py
+++ aurora/manager/core/sqldb_create.py
@@ -4,10 +4,13 @@
 """
 A Quick Script for creating the aurora database tables/schema
 """
-
+import logging
 import sys
+import traceback
 
 import MySQLdb as mdb
+
+LOGGER = logging.getLogger(__name__)
 
 class SQLDBCreate(object):
 
@@ -31,6 +34,7 @@ class SQLDBCreate(object):
         try:
             with self.con:
                 cur = self.con.cursor()
+                LOGGER.warn("Creating database")
                 cur.execute("CREATE DATABASE aurora")
 
         except mdb.Error, e:
@@ -44,31 +48,36 @@ class SQLDBCreate(object):
                 cur.execute("USE aurora")
 
                 #Create access_point table
+                LOGGER.warn("Creating table ap")
                 ap = "CREATE TABLE ap (name VARCHAR(255) NOT NULL PRIMARY KEY,region VARCHAR(255),firmware VARCHAR(255),\
  version VARCHAR(255),number_radio INT(11),memory_mb INT(11),free_disk INT(11),supported_protocol VARCHAR(255) DEFAULT 'a/b/g',\
  number_radio_free INT(11), number_slice_free INT(11), status ENUM('UP','DOWN','UNKNOWN'))"
                 cur.execute(ap)
 
                 #Create ap_slice table
+                LOGGER.warn("Creating table ap_slice")
                 ap_slice = "CREATE TABLE ap_slice (ap_slice_id VARCHAR(40) NOT NULL PRIMARY KEY, ap_slice_ssid VARCHAR(255), tenant_id VARCHAR(255),\
  physical_ap VARCHAR(255), project_id VARCHAR(255), wnet_id VARCHAR(40), status ENUM('PENDING','ACTIVE','FAILED','DOWN','DELETING','DELETED'),\
  time_active TIME DEFAULT '00:00:00', last_active_time DATETIME, mb_sent FLOAT DEFAULT 0.0)"
                 cur.execute(ap_slice)
 
                 #Create wnet table
+                LOGGER.warn("Creating table wnet")
                 wnet = "CREATE TABLE wnet( wnet_id VARCHAR(40) NOT NULL PRIMARY KEY, name VARCHAR(255) UNIQUE, tenant_id VARCHAR(255), project_id VARCHAR(40))"
                 cur.execute(wnet)
 
                 #Create location_tags table
+                LOGGER.warn("Creating table location_tags")
                 location_tags = "CREATE TABLE location_tags (name VARCHAR(255), ap_name VARCHAR(255), PRIMARY KEY(name, ap_slice_id))"
                 cur.execute(location_tags)
 
                 #Create tenant_tags table
+                LOGGER.warn("Creating table tenant_tags")
                 tenant_tags = "CREATE TABLE tenant_tags (name VARCHAR(255), ap_slice_id VARCHAR(40), PRIMARY KEY(name, ap_slice_id))"
                 cur.execute(tenant_tags)
 
         except mdb.Error, e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+            traceback.print_exc(file=sys.stdout)
 
 newDB = SQLDBCreate()
 newDB.create_database()
