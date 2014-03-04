cdd029dbf35e452225d980230e31a2cf32fa9993
Mike.Kobierski <michael.kobierski@mail.mcgill.ca>
2014-03-04 17:40:17 -0500

    Bug fix, aurora_db label

--- aurora/manager/core/aurora_db.py
+++ aurora/manager/core/aurora_db.py
@@ -266,7 +266,7 @@ class AuroraDB(object):
                                              CASE ap_slice.status
                                                  WHEN 'PENDING' THEN '%s'
                                                  WHEN 'DOWN' THEN '%s'
-                                             ELSE metering.last_time_activated END
+                                             ELSE metering.last_time_activated END,
                                          metering.last_time_updated=
                                              NULL
                                          WHERE metering.ap_slice_id='%s'""" %
