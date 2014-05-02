ec8d22b6daf583724d227b0d162fb8cad6cd4099
hptruong93 <hptruong93@gmail.com>
2014-05-02 10:46:05 -0400

    Bash script to backup and restore mysql database. Use of sqldb_create.py is not needed

---
+++ aurora/aurora/mysql_backup.sh
@@ -0,0 +1,17 @@
+#Take in either --backup or --restore
+#If backup then the script will try to backup the aurora database STRUCTURE only.
+#No data will be backup
+if [ "$1" == "--backup"  ] || [ "$1" == "-b" ]; then
+	mysqldump --no-data -u root -p aurora > aurora_backup.sql 
+
+#Otherwise the script expects the file "aurora_backup.sql" to load back to mysql database
+#If the file does not exist, the script ends by outputing a message saying the file does not exists
+#The aurora database will be created by the script if not exists
+elif [ "$1" == "--restore" ] || [ "$1" == "-r" ]; then
+	if [ -f aurora_backup.sql ]; then
+		mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS aurora"
+		mysql -u root -p aurora < aurora_backup.sql
+	else
+		echo "aurora_backup.sql does not exists. Recheck the directory"
+	fi
+fi
