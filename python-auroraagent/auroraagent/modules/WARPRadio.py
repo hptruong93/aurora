e5e170cd3004aa1bf28631e6ef9e769607378708
hptruong93 <hptruong93@gmail.com>
2015-01-13 12:02:14 -0500

    updating

--- python-auroraagent/auroraagent/modules/WARPRadio.py
+++ python-auroraagent/auroraagent/modules/WARPRadio.py
@@ -79,8 +79,11 @@ class WARPRadio:
 
         # we want to wait for the action to either complete or to come back as having not completed due to an error
         while (waiting_action["success"] != True) and (waiting_action["error"] == "") and \
-        ((int(round(time.time() * 1000)) - waiting_action["start_time"]) < self.action_timeout):
+        ((int(round(time.time() * 1000)) - waiting_action["start_time"]) < self.action_timeout*1000):
             time.sleep(self.sleep_time)
+
+        ln("time: %s\nstart_time: %s\ndifference: %s\ntimeout value: %s" % (int(round(time.time() * 1000)), waiting_action["start_time"], 
+            int(round(time.time() * 1000))- waiting_action["start_time"], self.action_timeout*1000))
 
         if waiting_action["error"] == "": # we have not RECEIVED an error, it may be that the error was a timeout on our end
             if waiting_action["success"]:          
